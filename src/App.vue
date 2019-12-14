8ffb45aa08bae0f0ddaa78ddb7adfe6dd7104388
cnxiahuajie <cnxiahuajie@icloud.com>
2019-12-14 19:42:56 +0800

    首页改版

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
     <div id="app">
         <div class="top">
-            <div class="left">
+            <div class="top-item left">
+                <div class="item collapse-container">
+                    <p class="collapse-button" @click="isCollapse = !isCollapse">
+                        <i :class="isCollapse? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
+                    </p>
+                </div>
                 <div class="item logo">
                     <p @click="toAbout">Vtarm library</p>
                 </div>
+            </div>
+            <div class="top-item center">
                 <div class="item search-box ">
                     <el-input v-model="query" placeholder="搜索" @change="toSearchResult(query)"></el-input>
                 </div>
             </div>
-            <div class="right link-box">
+            <div class="top-item right link-box">
                 <div class="item">
                     <el-link :underline="false">登录</el-link>
                 </div>
@@ -26,10 +33,13 @@
             </div>
         </div>
         <div class="main">
-            <div class="main-left">
-                <el-menu style="border-right: 0;">
+            <div class="main-left" :style="{width: isCollapse === true ? 'auto': '20%'}">
+                <el-menu style="border-right: 0;" :collapse="isCollapse">
                     <el-submenu :index="category.id"  v-for="category in categories" :key="category.id">
-                        <span slot="title" @click="toSearchResult(category.id)">{{category.name}}</span>
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span slot="title" @click="toSearchResult(category.id)">{{category.name}}</span>
+                        </template>
                         <el-menu-item-group v-if="category.columns">
                             <el-menu-item @click="toSearchResult(column.id)" :index="column.id" v-for="column in category.columns" :key="column.id">{{column.name}}</el-menu-item>
                         </el-menu-item-group>
@@ -53,7 +63,7 @@
         name: "App",
         data() {
             return {
-                isCollapse: false,
+                isCollapse: true,
                 query: '',
                 categories: []
             }
@@ -125,38 +135,63 @@
         overflow: hidden;
     }
 
-    #app .main .main-left {
-        width: 80%;
+    #app .top .top-item {
+        width: 100%;
         height: 100%;
+        flex-shrink: 1;
+    }
+
+    #app .main .main-left {
+        height: 100%;
+        flex-shrink: 0;
         border-right: 1px solid #DCDFE6;
     }
 
-    #app .main .main-left {
-        width: 20%;
-    }
-
     #app .main .main-right {
-        width: 80%;
+        width: 100%;
         padding: 10px;
     }
 
     #app .top .left {
         display: flex;
         align-items: center;
-        width: 50%;
+    }
+
+    #app .top .right {
+        display: flex;
+        justify-content: flex-end;
     }
 
     #app .top .left .logo {
         color: #FFF;
         display: flex;
         align-items: center;
-        width: 40%;
-        justify-content: center;
         text-shadow: 0 0 20px #FFFFFF;
+        height: 100%;
+    }
+
+    #app .top .left .collapse-container {
+        height: 100%;
+    }
+
+    #app .top .left .collapse-container .collapse-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 65px;
+        height: 100%;
+        margin: 0;
+        color: #FFFFFF;
+        transition: background-color 300ms, color 300ms;
+    }
+
+    #app .top .left .collapse-container .collapse-button:active {
+        background-color: #FFF;
+        color: #161616;
     }
 
     #app .search-box {
-        width: 300px;
+        width: 100%;
     }
 
     #app .link-box {
