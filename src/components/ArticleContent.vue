48d38e6dc2a4e78fbd28c8453d0e6ecbc0cce147
andy.sher <prerkt>
2019-12-27 11:31:41 +0800

    修复文章详情图片内容布局

--- src/components/ArticleContent.vue
+++ src/components/ArticleContent.vue
@@ -52,8 +52,8 @@
         }
 
         img {
-            width: 100%;
-            height: 100%;
+            max-width: 100%;
+            max-height: 100%;
             object-fit: scale-down;
         }
 
